72580a4660a5137e640967999bf392f316ed7176
Peter-Strbik <peterstrbik97@gmail.com>
2016-02-24 13:00:42 -0500

    Working better

--- Homework02/animate.js
+++ Homework02/animate.js
@@ -5,45 +5,43 @@ var stopButton = document.getElementById("stop");
 
 ctx.fillStyle = "#00ff00";
 
-var begin = false;
-var radius = 0;
-var ID;
-var expand = true;
 
-var start = function(){
-    begin = true;
-}
+
+
+var ID;
 
 var drawC = function(){
-    ctx.clearRect(0,0,500,500);
-    if (expand){
-	radius += 1;
-    }
-    else{
-	radius -= 1;
-    }
-    if (radius == c.height/2){
-	expand = false;
-    }
-    if (radius == 0){
-	expand = true;
-    }
+    window.cancelAnimationFrame(ID);
+    var radius = 0;
+    var expand = true;
+    var animate = function(){
+	ctx.clearRect(0,0,500,500);
+	if (expand){
+	    radius += 1;
+	}
+	else{
+	    radius -= 1;
+	}
+	if (radius == c.height/2){
+	    expand = false;
+	}
+	if (radius == 0){
+	    expand = true;
+	}
 
-    ctx.beginPath();
-    ctx.arc(c.height/2, c.width/2, radius, 0, 2 * Math.PI);
-    ctx.fill();
-    
-    ID = requestAnimationFrame(drawC);
+	ctx.beginPath();
+	ctx.arc(c.height/2, c.width/2, radius, 0, 2 * Math.PI);
+	ctx.fill();
+	
+	ID = requestAnimationFrame(animate);
+    };
+
+    animate()
 };
-
-if(begin){
-    ID = requestAnimationFrame(drawC);
-}
 
 var stopC = function(){
     cancelAnimationFrame(ID);
 };
 
 circleButton.addEventListener("click", drawC);
-circleButton.addEventListener("click", start);
 stopButton.addEventListener("click", stopC);
